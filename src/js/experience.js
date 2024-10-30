d222c730d3ac87d05439764a02577632f83c22da
Andy-J-B <080.abae@gmail.com>
2024-10-29 21:05:41 -0700

    Add js : change Experience functions for left and right click buttons

--- src/js/experience.js
+++ src/js/experience.js
@@ -11,7 +11,77 @@ experiences = {
 
 var id = 0;
 
-document.getElementById("experience-title").innerHTML = "";
+document.getElementById("experience-title").innerHTML = experiences[0]["title"];
+document.getElementById("experience-job-title").innerHTML =
+  experiences[0]["job_title"];
+document.getElementById("experience-date").innerHTML = experiences[0]["date"];
+document.getElementById("experience-languages-used").innerHTML =
+  experiences[0]["languages_used"];
+document.getElementById("experience-about").innerHTML = experiences[0]["about"];
 document.getElementById(
   "experience-picture"
 ).src = `../../imgs/pages/experience/experience-${id}.jpg`;
+
+function changeExperienceRight() {
+  id += 1;
+
+  if (checkIDRight() == false) {
+    return;
+  }
+
+  document.getElementById("experience-title").innerHTML =
+    experiences[id]["title"];
+  document.getElementById("experience-job-title").innerHTML =
+    experiences[id]["job_title"];
+  document.getElementById("experience-date").innerHTML = experiences[0]["date"];
+  document.getElementById("experience-languages-used").innerHTML =
+    experiences[id]["languages_used"];
+  document.getElementById("experience-about").innerHTML =
+    experiences[id]["about"];
+  document.getElementById(
+    "experience-picture"
+  ).src = `../../imgs/pages/experience/experience-${id}.jpg`;
+
+  return;
+}
+
+function checkIDRight() {
+  if (Object.keys(experiences).length == id) {
+    id -= 1;
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function changeExperienceLeft() {
+  id -= 1;
+
+  if (checkIDLeft() == false) {
+    return;
+  }
+
+  document.getElementById("experience-title").innerHTML =
+    experiences[id]["title"];
+  document.getElementById("experience-job-title").innerHTML =
+    experiences[id]["job_title"];
+  document.getElementById("experience-date").innerHTML = experiences[0]["date"];
+  document.getElementById("experience-languages-used").innerHTML =
+    experiences[id]["languages_used"];
+  document.getElementById("experience-about").innerHTML =
+    experiences[id]["about"];
+  document.getElementById(
+    "experience-picture"
+  ).src = `../../imgs/pages/experience/experience-${id}.jpg`;
+
+  return;
+}
+
+function checkIDLeft() {
+  if (Object.keys(experiences).length == -1) {
+    id += 1;
+    return false;
+  } else {
+    return true;
+  }
+}
