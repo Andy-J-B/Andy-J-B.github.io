25800e1bdd4447b70b2cf96203f96f3b31f0391a
Andy-J-B <080.abae@gmail.com>
2024-10-29 20:34:18 -0700

    Add all pictures for projects and experiences; add div structure

---
+++ src/js/experience.js
@@ -0,0 +1,17 @@
+experiences = {
+  0: {
+    title: "Hydrowave Android and IOS application",
+    job_title: "Freelance Developer",
+    date: "July - October 2024",
+    languages_used: "Flutter, Dart, and Firebase",
+    about:
+      "Successfully developed flutter application for Hydrowave to improve sales. Collaborated with developer and product manager to create an ecommerce application available on android and ios. Debugged and tested flutter based software to increase software robustness.",
+  },
+};
+
+var id = 0;
+
+document.getElementById("experience-title").innerHTML = "";
+document.getElementById(
+  "experience-picture"
+).src = `../../imgs/pages/experience/experience-${id}.jpg`;
